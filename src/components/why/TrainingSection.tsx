f26f539a7e63e4d404156a8903e63d557c1f34a9
Siddhant1610 <kumarsiddhant1610@gmail.com>
2025-07-11 11:00:43 +0530

    image issue solved

--- src/components/why/TrainingSection.tsx
+++ src/components/why/TrainingSection.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const TrainingSection = () => {
   return (
     <>
-    <section className="w-full bg-[url('/images/trueOpsTrainingbg.jpg')] bg-cover bg-no-repeat bg-center py-10">
-      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row items-center justify-center gap-10 lg:gap-80">
+    <section className="w-full bg-[url('/images/banner.jpg')] bg-cover bg-no-repeat bg-center  pt-10 pb-32">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row items-center  gap-10 lg:gap-80">
         <div className="text-center md:text-left">
           <h1 className="text-4xl sm:text-5xl font-bold py-2">From</h1>
           <h2 className="text-4xl sm:text-5xl font-bold">Novice to</h2>
@@ -15,15 +15,15 @@ const TrainingSection = () => {
             through hands-on experience.
           </p>
         </div>
-        <div className="flex justify-center">
+        {/* <div className="flex justify-center">
           <Image
-            src="/images/TrueOpsHIW4.jpg"
+            src="/images/TrueOpsHIW4."
             alt="Training"
             width={240}
             height={240}
             className="w-60 object-contain"
           />
-        </div>
+        </div> */}
       </div>
     </section>
       <section className="flex items-center justify-center py-10 px-4">
