7ffdd4e4d83fe826b38939c390ea1e9e0ad2dd1f
suryanshuPal1 <suryanshu.jschamps@gmail.com>
2025-07-12 12:29:49 +0530

    about us

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
 import AboutSection from '@/components/about/AboutSection';
 import DevOpsSection from '@/components/about/DevOpsSection';
 import Footer from '@/components/home/Footer';
 import NavBar from '@/components/home/Navbar';
 
-interface AboutProps {
-  sections: {
-    header: any[];
-    header2: any[];
-    Middle: any[];
-    lowerMiddle: any[];
+async function getData() {
+  const domain = 'Truops.in';
+  const page = 'About';
+  
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/static?domain=${domain}&page=${encodeURIComponent(page)}`, {
+    cache: 'no-store'
+  });
+
+  if (!res.ok) {
+    const errorText = await res.text();
+    console.error('API error response:', errorText);
+    throw new Error('Failed to fetch page data');
+  }
+
+  const data = await res.json();
+  const all = data?.sections ?? [];
+
+  return {
+          header: all.find((s: any) => s.name === 'Header')?.contents || [],
+          header2: all.find((s: any)=> s.name === 'Header2')?.contents || [],
+          Middle: all.find((s: any)=> s.name === 'Middle')?.contents || [],
+          lowerMiddle: all.find((s: any)=> s.name === 'LowerMiddle')?.contents || []
   };
 }
 
-const About: React.FC<AboutProps> = ({ sections }) => {
+export default async function About (){
+  const sections = await getData()
   return (
     <>
       <NavBar />
@@ -32,4 +48,4 @@ const About: React.FC<AboutProps> = ({ sections }) => {
   );
 };
 
-export default About;
+
